91255c6b78f82b579e1ee7747b8b4a4fc86c0881
khandu kore <khandu261@gmail.com>
2022-07-13 13:57:15 +0530

    this change from khandu

---
+++ CharArray.java
@@ -0,0 +1,23 @@
+package arrays;
+
+public class CharArray {
+
+	public static void main(String[] args) 
+	{
+	// 1. array decleration
+		char grade[]=new char[5];
+		
+	//2. array initilization
+		grade[0]='a';
+		grade[1]='b';
+		grade[2]='c';
+		grade[3]='d';
+		grade[4]='e';
+		
+		//3. usage
+for(int i=0;i<=grade.length-1;i++) {
+	System.out.println(grade[i]);
+}			
+	}
+}
+//Nilesh use trtee diametiopnal arrays
